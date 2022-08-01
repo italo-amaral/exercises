8b943846b17dc64463bd95c9a776181d492685e2
Italo Amaral <italocoelho57@gmail.com>
2022-08-01 19:50:58 -0300

    alteração tipo objetos

--- palindromo/palindromo.js
+++ palindromo/palindromo.js
@@ -1,13 +1,13 @@
-var teste0 = "OvO";
-var teste1 = "A base DO teto desaba";
-var teste2 = "dois tigres tristes";
-function testarPalindromo(text){
-    var array = Array.from(text.toLowerCase().replace(/ /g,"")); // elimina espaços em branco e letras maiusculas
-    var arrayReverso = [];
+const teste0 = "OvO";
+const teste1 = "A base DO teto desaba";
+const teste2 = "dois tigres tristes";
+function isPalindromo(text){
+    let array = Array.from(text.toLowerCase().replace(/ /g,"")); // elimina espaços em branco e letras maiusculas
+    let arrayReverso = [];
     for (let i = array.length - 1; i >= 0; i--){ //cria uma arrayReverso com os itens da array original de trás pra frente
         arrayReverso.push(array[i]); 
     }
-    var count = 0;
+    let count = 0;
     for (let x = 0; x < array.length; x++){ //testa se os itens das duas arrays são iguais, através de um contador
         if (array[x] === arrayReverso[x]) { //se count += 1 -> não é palindromo
             count += 0
@@ -23,6 +23,6 @@ function testarPalindromo(text){
         return "Não é um palindromo"
     }
 }
-console.log(testarPalindromo(teste0))
-console.log(testarPalindromo(teste1))
-console.log(testarPalindromo(teste2))
+console.log(isPalindromo(teste0))
+console.log(isPalindromo(teste1))
+console.log(isPalindromo(teste2))
