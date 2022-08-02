9e1d9183e693b9edfbb8de69738b55c3f7fbdd6f
Italo Amaral <italocoelho57@gmail.com>
2022-08-01 21:35:22 -0300

    quebrando a função em duas

--- palindromo/palindromo.js
+++ palindromo/palindromo.js
@@ -1,15 +1,21 @@
 const teste0 = "OvO";
 const teste1 = "A base DO teto desaba";
 const teste2 = "dois tigres tristes";
+
+function convertArray(arr){
+    arr = Array.from(arr.toLowerCase().replace(/ /g,"")); // elimina espaços em branco e letras maiusculas
+    return arr
+}
+
 function isPalindromo(text){
-    let array = Array.from(text.toLowerCase().replace(/ /g,"")); // elimina espaços em branco e letras maiusculas
+    text = convertArray(text)
     let arrayReverso = [];
-    for (let i = array.length - 1; i >= 0; i--){ //cria uma arrayReverso com os itens da array original de trás pra frente
-        arrayReverso.push(array[i]); 
+    for (let i = text.length - 1; i >= 0; i--){ //cria uma arrayReverso com os itens da array original de trás pra frente
+        arrayReverso.push(text[i]); 
     }
     let count = 0;
-    for (let x = 0; x < array.length; x++){ //testa se os itens das duas arrays são iguais, através de um contador
-        if (array[x] === arrayReverso[x]) { //se count += 1 -> não é palindromo
+    for (let x = 0; x < text.length; x++){ //testa se os itens das duas arrays são iguais, através de um contador
+        if (text[x] === arrayReverso[x]) { //se count += 1 -> não é palindromo
             count += 0
         }
         else{
