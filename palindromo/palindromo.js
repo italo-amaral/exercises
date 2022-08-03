ee1e846f808ff49bc607f4d0b8d1c2d812fe0f2f
Italo Amaral <italocoelho57@gmail.com>
2022-08-02 22:06:13 -0300

    refatoração das funções: código agora atende a strings e listas

--- palindromo/palindromo.js
+++ palindromo/palindromo.js
@@ -1,30 +1,28 @@
-const testeCase = ['ovo', 'A base do teto desaba', 'dois tigres tristes']
-const teste0 = "OvO";
-const teste1 = "A base DO teto desaba";
-const teste2 = "dois tigres tristes";
-const teste3 = "A base Da teto desaba";
+const testCase = ['ovo', 'A base do teto desaba', 'dois tigress tristes']
+const test0 = "OvO";
+const test1 = "A base DO teto desaba";
+const test2 = "dois tigres tristes";
 
-function convertArray(arr){ // elimina espaços em branco e letras maiusculas
-    if (arr instanceof Object){
-        for (let i = 0; i < arr.length; i++){
-            arr[i] = Array.from(arr[i].toLowerCase().replace(/ /g,""));
-        }
-    }else{
-        arr = Array.from(arr.toLowerCase().replace(/ /g,""));
+function convertArray(str){ // transforma string em array (caso entrada for string) e elimina espaços em branco e letras maiusculas
+    let arr = typeof(str) === 'string' ? [str] : str
+    console.log(arr)
+    for (let i = 0; i < arr.length; i++){
+        arr[i] = Array.from(arr[i].toLowerCase().replace(/ /g,""));
     }
     return arr
 }
-
-function isPalindromo(text){
+function isPalindromo(text){ // testa se a string é um palindromo
     text = convertArray(text)
-    let arrayReverso = [];
-    for (let x = 0; x < text.length; x++){ 
-        arrayReverso.push(text[x]); //cria uma arrayReverso com os itens da array original de trás pra frente
-        if (text[text.length-1-x] !== arrayReverso[x]){ 
-            return 'não é um palindromo'
+    let result = []
+    for (let i = 0; i < text.length; i++){
+        for (let x = 0; x < text[i].length; x++){ 
+            result[i] = 'É um palindromo'
+            if (text[i][text[i].length-1-x] !== text[i][x]){ 
+                result[i] = 'Não é um palindromo'
+            }
         }
     }
-    return "É um palindromo"
-    }
-    
-console.log(isPalindromo(teste3))
+    return result
+}
+
+console.log(isPalindromo(testCase))
