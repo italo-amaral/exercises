24a0e89244cb113b2e07bb2d5f227b3a4704f3a4
Italo Amaral <italocoelho57@gmail.com>
2022-08-12 21:05:18 -0300

    adicionado módulo Argparse e realizado pequenos ajustes. V0

--- bibiloteca_central_ideias/main.py
+++ bibiloteca_central_ideias/main.py
@@ -1,17 +1,26 @@
 import requests
 import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(description="search for books in the collection 'Biblioteca Central de Ideias")
+parser.add_argument('-a', '--author', type=str, metavar='', help='search through author')
+parser.add_argument('-t', '--title', type=str, metavar='', help='search through title')
+parser.add_argument('-g', '--genre', type=str, metavar='', help='search through GENRE')
+args = parser.parse_args()
 
 MAX_ROWS = 100
 MAX_COLUMNS = 4
-WIDTH = 1000
+WIDTH = 10000
 pd.set_option('display.max_rows', MAX_ROWS)
 pd.set_option('display.max_columns', MAX_COLUMNS)
 pd.set_option('display.width', WIDTH)
 
 link = ['http://www.institutousiminas.com/wp-content/uploads/2020/08/Adm-Publica.-Servico-Social.xlsx',
-        'https://www.institutousiminas.com/wp-content/uploads/2020/08/Historia-e-Geografia.xlsx']
-link2 = ['https://www.institutousiminas.com/wp-content/uploads/2020/08/Administracao.xlsx',
+        'https://www.institutousiminas.com/wp-content/uploads/2020/08/Historia-e-Geografia.xlsx',
+        'https://www.institutousiminas.com/wp-content/uploads/2020/08/Administracao.xlsx',
         'https://www.institutousiminas.com/wp-content/uploads/2020/08/Infantil-e-Juvenil.xlsx']
+link2 = ['http://www.institutousiminas.com/wp-content/uploads/2020/08/Adm-Publica.-Servico-Social.xlsx',
+        'https://www.institutousiminas.com/wp-content/uploads/2020/08/Historia-e-Geografia.xlsx']
 def create_df(link_bs):
     response = requests.get(link_bs, verify=False)
     file_name = link_bs.split('/')[-1]
@@ -20,12 +29,13 @@ def create_df(link_bs):
     df = pd.read_excel(file_name)
     df = df.dropna(how='all', axis=1)  # remove colunas que todos os valores sejam vazios
     df = df.ffill()                    # preenche linhas vazias com o valor da linha superior
-    #df = df.dropna(how='any', axis=0)
-    df.loc[:, 'GENERO'] = file_name.split('.')[-2]   # adiciona coluna com o genero do livro
-    for i in range(0, len(df['Nº de Chamada']) - 1):       # concatena titulos iguais baseados no id
-        if df['Nº de Chamada'][i] == df['Nº de Chamada'][i + 1] and df['TÍTULO'][i] != df['TÍTULO'][i+1]:
-            df['TÍTULO'][i + 1] = str(df['TÍTULO'][i]) + str(df['TÍTULO'][i + 1])
-    df = df.drop_duplicates(subset='Nº de Chamada', keep='last')  # remove linhas duplicadas a partir do id
+    df.columns = ['ID', 'TITLE', 'AUTHOR'] 
+    df.loc[:, 'GENRE'] = file_name.split('.')[-2]   # adiciona coluna com o genero do livro
+    #df.groupby('ID')['TITLE'].apply(lambda line: "".join(line))   #estudar o metodo
+    for i in range(0, len(df['ID']) - 1):       # concatena titulos iguais baseados no id
+        if df['ID'][i] == df['ID'][i + 1] and df['TITLE'][i] != df['TITLE'][i+1]:
+            df['TITLE'][i + 1] = str(df['TITLE'][i]) + str(df['TITLE'][i + 1])
+    df = df.drop_duplicates(subset='ID', keep='last')  # remove linhas duplicadas a partir do id
     return df
 
 def create_general_df(links):
@@ -34,5 +44,13 @@ def create_general_df(links):
         general_df = general_df.append(create_df(links[i]))
     return general_df
 
-#print(create_df(link[1]))
-print(create_general_df(link))
+library_df = create_general_df(link)
+if __name__ == '__main__':
+    if args.author:
+        print(library_df.loc[library_df['AUTHOR'] == args.author])
+    elif args.title:
+        print(library_df.loc[library_df['TITLE'] == args.title])
+    elif args.genre:
+        print(library_df.loc[library_df['GENRE'] == args.genre])
+
+
