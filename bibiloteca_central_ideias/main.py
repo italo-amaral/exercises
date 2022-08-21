99612a1d0222be47bf75c48d04430b4b80f5cbaf
Italo Amaral <109629036+italo-amaral@users.noreply.github.com>
2022-08-21 12:41:23 -0300

    adicionado link geral

--- bibiloteca_central_ideias/main.py
+++ bibiloteca_central_ideias/main.py
@@ -8,7 +8,7 @@ parser.add_argument('-t', '--title', metavar='', help='search through TITLE')
 parser.add_argument('-g', '--genre', metavar='', help='search through GENRE')
 args = parser.parse_args()
 
-MAX_ROWS = 100
+MAX_ROWS = 10000
 MAX_COLUMNS = 4
 WIDTH = 1000
 pd.set_option('display.max_rows', MAX_ROWS)
@@ -43,8 +43,6 @@ link_geral = ['https://www.institutousiminas.com/wp-content/uploads/2020/08/Adm-
 'https://www.institutousiminas.com/wp-content/uploads/2020/08/Sexualidade.xlsx',
 'https://www.institutousiminas.com/wp-content/uploads/2020/08/Sociologia.xlsx']
 
-link2= []
-
 def create_df(link_bs):
     response = requests.get(link_bs, verify=False)
     file_name = link_bs.split('/')[-1]
@@ -54,7 +52,7 @@ def create_df(link_bs):
     df = df.dropna(how='all', axis=1)  # remove colunas que todos os valores sejam vazios
     df = df.ffill()                    # preenche linhas vazias com o valor da linha superior
     df.columns = ['ID', 'TITLE', 'AUTHOR'] 
-    df.loc[:, 'GENRE'] = file_name.split('.')[-2]   # adiciona coluna com o genero do livro
+    df.loc[:, 'GENRE'] = file_name.split('.')[0].replace('-', ' ')   # adiciona coluna com o genero do livro
     #df.groupby('ID')['TITLE'].apply(lambda line: "".join(line))   #estudar o metodo
     for i in range(0, len(df['ID']) - 1):       # concatena titulos iguais baseados no id
         if df['ID'][i] == df['ID'][i + 1] and df['TITLE'][i] != df['TITLE'][i+1]:
@@ -68,8 +66,8 @@ def create_general_df(link):
         general_df = pd.concat([general_df, create_df(link[i])], axis=0, ignore_index=True)
     return general_df
 
-library_df = create_general_df(link_geral)
-print(library_df)
+library_df = create_general_df(link_test)
+#print(library_df)
 
 if __name__ == '__main__':
     if args.author:
@@ -78,5 +76,3 @@ if __name__ == '__main__':
         print(library_df.loc[library_df['TITLE'] == args.title])
     elif args.genre:
         print(library_df.loc[library_df['GENRE'] == args.genre])
-
-
